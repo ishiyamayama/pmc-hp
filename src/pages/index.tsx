9043a15c57e947dfc03ed062713bc238f2c784a0
daisuke ishiyama <disk.yama.o7z8@gmail.com>
2022-06-28 22:59:23 +0900

    remを調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,7 @@ export const getStaticProps = async () => {
   const { links } = await fetchLinks()
   return {
     props: { posts, bio, dataTable, photos, links },
+    revalidate: 120,
   }
 }
 
