43e3b42e1a20c6bec2d97369da85eca713431f9e
daisuke ishiyama <disk.yama.o7z8@gmail.com>
2022-07-11 10:29:26 +0900

    update

--- src/components/atoms/CategoryButton.tsx
+++ src/components/atoms/CategoryButton.tsx
@@ -8,13 +8,14 @@ type PropsType = {
 
 export const CategoryButton = ({ categoryName, onClick, current, order, disabled }: PropsType) => (
   <button
-    className={`relative p-[.2rem_.6rem_.3rem_.6rem] rounded-lg leading-[1.2] hover:opacity-60
-    ${current ? 'bg-white' : 'bg-[#E6E6E6]'} ${disabled ? 'opacity-60 !bg-[#E6E6E6]' : ''}`}
+    className={`relative p-[.2rem_.6rem_.3rem_.6rem] leading-[1.2] hover:opacity-60
+    ${current ? 'opacity-80 hover:opacity-80' : ''} ${disabled ? 'opacity-60 !bg-[#E6E6E6]' : ''}`}
     onClick={onClick}
     style={{ order }}
     disabled={disabled}
   >
-    <span className='absolute top-0 left-0 w-full h-full border border-current rounded-lg opacity-50' />
-    {categoryName}
+    <span className={`absolute top-0 left-0 w-full h-full border border-current rounded-lg opacity-40 bg-gray
+    ${current ? 'border-0' : ''}`} />
+    <span className='relative z-10'>{categoryName}</span>
   </button>
 )
--- src/components/molecules/PostRow.tsx
+++ src/components/molecules/PostRow.tsx
@@ -1,6 +1,6 @@
 import NextLink from 'next/link'
 import { categoryColors } from 'const/categoryColors'
-import style from 'styles/modules/body.module.sass'
+import style from 'styles/modules/article.module.sass'
 import { PostContentType } from 'types'
 
 type PropsType = {
@@ -18,12 +18,12 @@ export const PostRow = ({ post, currentPost }: PropsType) => {
           <time>{dateString}</time>
           <h2>{title}</h2>
           <span>{category.name}</span>
-          <span className='min-w-[1.5rem]'>
-            <img className='w-[1.5rem] block object-cover aspect-[1/1]' src={coverImage.src} alt='' />
+          <span className='min-w-[1em] flex'>
+            <img className='w-full max-w-[1.25em] block object-cover aspect-[1/1]' src={coverImage.src} alt='' />
           </span>
-          <span className='min-w-[1.5rem]'>
+          <span className='min-w-[1em] flex'>
             <span
-              className={`ml-auto w-[1.5rem] block object-cover aspect-[1/1] bg-${categoryColors[category.name]}`}
+              className={`ml-auto w-full max-w-[1.25em] block object-cover aspect-[1/1] bg-${categoryColors[category.name]}`}
             />
           </span>
         </a>
