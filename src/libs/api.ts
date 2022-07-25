9f793412fd4e483a66748e0c373f1079bc1e1ec5
daisuke ishiyama <disk.yama.o7z8@gmail.com>
2022-07-25 00:58:32 -0600

    update

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -31,10 +31,11 @@ export const fetchPosts = async () => {
   })
   items.sort((a, b) => (a.date > b.date ? -1 : 1))
 
-  // console.log(utcToZonedTime(items[0].date, 'Asia/Tokyo'));
+  console.log(utcToZonedTime(items[0].date, 'Asia/Tokyo'));
   items.map((item: PostContentType) => {
     const d = utcToZonedTime(item.date, 'Asia/Tokyo')
-    item.date = dayjs.utc(d).tz().format('YYYY/MM/DD')
+    item.date = dayjs.utc(d).tz('Asia/Tokyo').format('YYYY/MM/DD')
+    console.log(item.date);
   })
   return { posts: items }
 }
